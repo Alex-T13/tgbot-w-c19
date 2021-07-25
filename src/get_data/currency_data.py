e3efa03b71b455186d5b34d9211f1655e5b7217f
Alexander-PC <morphix313@gmail.com>
2021-07-25 13:07:40 +0300

    fix currency (nbrb)

--- src/get_data/currency_data.py
+++ src/get_data/currency_data.py
@@ -17,16 +17,19 @@ async def currency_data(args: FuncParameters) -> Optional[str]:
 
     payload = await response.json()
     new_dict_resp = {
-        'USD': 145,
-        'EUR': 292,
-        '100 RUB': 298,
-        '100 UAH': 290,
-        '10 PLN': 293,
+        'USD': 431,  #145,
+        'EUR': 451,  #292,
+        '100 RUB': 456,  #298,
+        '100 UAH': 449,  #290,
+        '10 PLN': 452,  #293,
     }
 
-    for key in new_dict_resp:
-        curr_filter = list(filter(lambda cur: cur['Cur_ID'] == new_dict_resp.get(key), payload))[-1]['Cur_OfficialRate']
-        new_dict_resp[key] = curr_filter
-
-    return Translator.data_translation(loc=args.localization, data=new_dict_resp)
+    try:
+        for key in new_dict_resp:
+            curr_filter = list(filter(lambda cur: cur['Cur_ID'] == new_dict_resp.get(key), payload))[-1]['Cur_OfficialRate']
+            new_dict_resp[key] = curr_filter
+    except IndexError:
+        return None
+    else:
+        return Translator.data_translation(loc=args.localization, data=new_dict_resp)
 
